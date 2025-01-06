4073a5722f93d903551d236e702ff0f6356e4328
Drj2001 <107933233+Darshanrgowda54@users.noreply.github.com>
2025-01-06 09:54:23 +0530

    main file

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "mainmenu.h"
+
+int main()
+{
+    mainMenu();
+    return 0;
+}
+
---
+++ mainMenu.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "admin.h"
+#include "user.h"
+#include "fileoperations.h"
+
+enum mainMenuOption
+{
+    ADMIN = 1,
+    USER,
+    EXIT
+};
+
+int mainMenu()
+{
+    user *head = NULL;
+
+    // head = loadAccountsFromFile(head);
+    // loadTransactionsFromFile(head);
+
+    head = loadUserDataFromFile("users.txt", "transactions.txt");
+
+    int choice;
+
+    while (1)
+    {
+        printf("\n________ Bank Management System ________\n");
+        printf("1. Admin Login\n");
+        printf("2. User Login\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case ADMIN:
+            if (adminLogin())
+            {
+                printf("Admin login Successful.\n");
+                head = adminMenu(head);
+            }
+            else
+            {
+                printf("Invalid admin credentials.\n");
+            }
+            break;
+
+        case USER:
+        {
+            user *loggedInUser = userLogin(head);
+            if (loggedInUser != NULL)
+            {
+                userMenu(loggedInUser, head);
+            }
+            break;
+        }
+
+        case EXIT:
+
+            saveUserDataToFile(head, "users.txt", "transactions.txt");
+            printf("Thank you. Please visit again\n");
+            free(head);
+            return 0;
+
+        default:
+            printf("Invalid choice. Please try again.\n");
+            break;
+        }
+    }
+}
